bf339f7144ba4e25d953cde0186e5bb31680a6a2
Benjamin Chamness <bjchamness@yahoo.com>
2021-03-31 11:43:54 -0400

    ticket class

---
+++ TixFix.Data/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TixFix.Data
+{
+    public class Ticket
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+                
+        [Display(Name ="Opponent")]
+        public int OpponentId { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public DateTime DateOfGame { get; set; }
+
+        [Required]
+        public bool IsAvailable { get; set; }
+
+    }
+}
