7a82302a0ddbda63c0dca93c6092146edde0b64c
Benjamin Chamness <bjchamness@yahoo.com>
2021-03-31 11:34:17 -0400

    Add project files.

---
+++ TixFix.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TixFix.WebMVC.Startup))]
+namespace TixFix.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
