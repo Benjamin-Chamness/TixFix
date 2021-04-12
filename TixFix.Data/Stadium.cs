27e864236d6bcc22708a73da90b3b657119cbe06
Benjamin Chamness <bjchamness@yahoo.com>
2021-04-12 10:43:39 -0400

    stadium table/styling

---
+++ TixFix.Data/Stadium.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TixFix.Data
+{
+    public class Stadium
+    {
+        [Key]
+        public int StadiumId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [ForeignKey(nameof(Opponent))]
+        public int EventId { get; set; }
+        public virtual Opponent Opponent { get; set; }
+
+    }
+}
---
+++ TixFix.WebMVC/Controllers/StadiumController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TixFix.Models;
+
+namespace TixFix.WebMVC.Controllers
+{
+    [Authorize]
+    public class StadiumController : Controller
+    {
+        // GET: Stadium
+        public ActionResult Index()
+        {
+            var model = new StadiumListItem[0];
+            return View(model);
+        }
+    }
+}
